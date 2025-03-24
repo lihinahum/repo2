19e5a300ab80db04c6a15865d15d2d3b0e62fa27
lihinahum <lihina4@gmail.com>
2025-03-24 11:09:21 +0200

    chenged the print message in Lihi.java

--- Lihi.java
+++ Lihi.java
@@ -1,7 +1,7 @@
 public class Lihi{
           public static void main(String[] args){
                             for (int i=1;i<=10;i++) {
-                                       System.out.println("Lihi"+i);
+                                       System.out.println("Lihi Nahum"+i);
                                        }
         }
 }
