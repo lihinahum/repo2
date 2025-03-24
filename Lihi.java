b9aa46eb71688bd10a953e497c8ff87f1860a388
lihinahum <lihina4@gmail.com>
2025-03-24 10:20:24 +0200

    added new files hello and lihi

---
+++ Lihi.java
@@ -0,0 +1,35 @@
+public class Lihi{
+          public static void main(String[] args){
+                            for (int i=1;i<=10;i++) {
+                                       System.out.println("Lihi"+i);
+                                       }
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
